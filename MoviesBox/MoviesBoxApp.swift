1063c0955d95b7afe72828e7f48ce4b65cf8dd05
george <george.magdy@thirdwayv.com>
2025-08-15 07:20:49 +0300

    Initial Commit

---
+++ MoviesBox/MoviesBoxApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoviesBoxApp.swift
+//  MoviesBox
+//
+//  Created by George Magdy on 15/08/2025.
+//
+
+import SwiftUI
+
+@main
+struct MoviesBoxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
